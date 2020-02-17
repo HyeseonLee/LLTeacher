69e5bec9f11e7c574a2d5416dd91f233c6c44882
Hyeseon Lee <hyesun1999@gmail.com>
2020-02-18 03:06:17 +0900

    [Back-end] feat : Add GeneralChemistry2 model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class GeneralChemistry2(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, default =1, on_delete = models.CASCADE)
+    lectureName = models.CharField(max_length=200)
+    professorName = models.CharField(max_length=50)
+    seme = models.CharField(max_length=10, choices=SEME_FIELD_CHOICES)
+    SEME_FIELD_CHOICES=[
+        ('19년도 2학기', '19년도 2학기'),
+        ('19년도 1학기', '19년도 1학기'),
+        ('18년도 2학기', '18년도 2학기'),
+        ('18년도 2학기', '18년도 1학기'),
+    ]
+    score = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)], default=0])
+    text = models.TextField(max_length=300)
