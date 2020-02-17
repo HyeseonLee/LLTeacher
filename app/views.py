32f02f2015676ec2fcc7adda119e008e23a7ee86
Hyeseon Lee <hyesun1999@gmail.com>
2020-02-18 03:11:18 +0900

    [Back-end] feat : Add GC2 Listview

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import GeneralChemistry2
 
 # Create your views here.
+class GC2LV(ListView):
+    model = GeneralChemistry2
+    template_name = 'GeneralChemistry2.html'
+
