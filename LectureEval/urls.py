eb3a7fd850b9acd768352ebdf31d4a3e7c6a6c03
seulong <kjkj3468@naver.com>
2020-02-18 04:15:48 +0900

    [Back-end] Fix : modles.py default = [0] , SEME_FIELD_CHOICES 를 먼저 선언하게 위치 변경(makemigrations 오류관련) ,  settings INSTALLED_APPS change.

--- LectureEval/urls.py
+++ LectureEval/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import GC2Read #Read기능을 하는 ListView들 import하기
+from app import views
 
 urlpatterns = [
-    path('', name='main'),
+    #path('', name='main'),
     path('GC2/', GC2Read.as_view(), name='GC2'),
     path('admin/', admin.site.urls),
-
 ]
