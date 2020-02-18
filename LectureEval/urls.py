1dd260bce18045b3a35bd619971f546701337121
SeaJungg <wantedsaea@gmail.com>
2020-02-18 15:56:04 +0900

    [Back-end] fix : 전과목 리스트뷰 추가

--- LectureEval/urls.py
+++ LectureEval/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 import app.views
-from app.views import GC2Read #Read기능을 하는 ListView들 import하기
+from app.views import GC2Read, LERead, PERead, WPRead #Read기능을 하는 ListView들 import하기
 
 
 urlpatterns = [
     path('', app.views.index, name='main'),
-    path('GC2/', GC2Read.as_view(), name='GC2'),
     path('admin/', admin.site.urls, name="admin"),
+    path('GC2/', GC2Read.as_view(), name='GC2'),
+    path('LE/', LERead.as_view(), name='LE'),
+    path('PE/', PERead.as_view(), name='PE'),
+    path('WP/', WPRead.as_view(), name='WP'),
 ]
 
