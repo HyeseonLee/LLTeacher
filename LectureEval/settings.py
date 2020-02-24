83c1a923092e264216d91c1f28f569862f6c6c44
TaeWonJin <wlsxodnjs1@naver.com>
2020-02-25 00:02:29 +0900

    [ETC]YEAH!

--- LectureEval/settings.py
+++ LectureEval/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = '*'
 
